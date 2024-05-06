1e5036ccd703f138e34de8e7bbe3847adfe23425
DongHungFeng <donghung.feng@gmail.com>
2024-05-06 23:37:21 +0700

    fix: quantity pet in cage

--- src/main/java/com/example/jingangfarmmanagement/service/Impl/StatisticServiceImpl.java
+++ src/main/java/com/example/jingangfarmmanagement/service/Impl/StatisticServiceImpl.java
@@ -2,7 +2,10 @@ package com.example.jingangfarmmanagement.service.Impl;
 
 import com.example.jingangfarmmanagement.model.BaseResponse;
 import com.example.jingangfarmmanagement.model.req.StatisticPetWithAge;
+import com.example.jingangfarmmanagement.repository.entity.Pet;
 import com.example.jingangfarmmanagement.service.*;
+import com.example.jingangfarmmanagement.uitl.SpecUtil;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 
@@ -10,7 +13,6 @@ import org.springframework.stereotype.Service;
 public class StatisticServiceImpl implements StatisticService {
     @Override
     public BaseResponse statisticPetWithAge(StatisticPetWithAge statisticPetWithAge) {
-
         return new BaseResponse().success("ok");
     }
 }
