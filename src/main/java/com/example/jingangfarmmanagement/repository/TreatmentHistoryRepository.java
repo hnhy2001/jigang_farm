2aadac69a85a7d69ebd2eb10c80a269637e08892
HoangDuyDatDev <hoangduydat10@gmail.com>
2024-05-31 00:03:23 +0700

    [create] create get TreatmentHistory by pet

--- src/main/java/com/example/jingangfarmmanagement/repository/TreatmentHistoryRepository.java
+++ src/main/java/com/example/jingangfarmmanagement/repository/TreatmentHistoryRepository.java
@@ -1,8 +1,25 @@
 package com.example.jingangfarmmanagement.repository;
 
 import com.example.jingangfarmmanagement.repository.entity.TreatmentHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TreatmentHistoryRepository extends BaseRepository<TreatmentHistory> {
+    @Query(value = "SELECT th.* \n" +
+            "FROM treatment_card tc JOIN treatment_card_pet tcp ON tc.id = tcp.treatment_card_id \n" +
+            "JOIN treatment_history th ON th.treatment_card_id = tc.id\n" +
+            "JOIN pet p ON p.id = tcp.pet_id \n" +
+            "WHERE p.id IN :petIds",
+            countQuery = "SELECT COUNT(th.*) \n" +
+                    "FROM treatment_card tc JOIN treatment_card_pet tcp ON tc.id = tcp.treatment_card_id \n" +
+                    "JOIN treatment_history th ON th.treatment_card_id = tc.id\n" +
+                    "JOIN pet p ON p.id = tcp.pet_id \n" +
+                    "WHERE p.id IN :petIds",
+            nativeQuery = true)
+    Page<TreatmentHistory> findTreatmentHistoriesByPet(List<Long> petIds, Pageable pageable);
 }
