4545ae5c9347e479fec4d3e45eedb38e591f55a3
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2024-06-12 00:40:38 +0700

    fix syntax

--- src/main/java/com/example/jingangfarmmanagement/repository/TreatmentCardRepository.java
+++ src/main/java/com/example/jingangfarmmanagement/repository/TreatmentCardRepository.java
@@ -4,20 +4,22 @@ import com.example.jingangfarmmanagement.repository.entity.TreatmentCard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TreatmentCardRepository extends BaseRepository<TreatmentCard> {
-    @Query(value = "SELECT tc.* \n" +
-            "FROM treatment_card tc JOIN treatment_card_pet tcp ON tc.id = tcp.treatment_card_id \n" +
-            "JOIN pet p ON p.id = tcp.pet_id \n" +
+    @Query(value = "SELECT tc.* " +
+            "FROM treatment_card tc JOIN treatment_card_pet tcp ON tc.id = tcp.treatment_card_id " +
+            "JOIN pet p ON p.id = tcp.pet_id " +
             "WHERE p.id IN :petIds",
-            countQuery = "SELECT COUNT(tc.*) \n" +
-                    "FROM treatment_card tc JOIN treatment_card_pet tcp ON tc.id = tcp.treatment_card_id \n" +
-                    "JOIN pet p ON p.id = tcp.pet_id \n" +
+            countQuery = "SELECT COUNT(*) " +
+                    "FROM treatment_card tc JOIN treatment_card_pet tcp ON tc.id = tcp.treatment_card_id " +
+                    "JOIN pet p ON p.id = tcp.pet_id " +
                     "WHERE p.id IN :petIds",
             nativeQuery = true)
-    Page<TreatmentCard> findTreatmentCardsByPet(List<Long> petIds, Pageable pageable);
+    Page<TreatmentCard> findTreatmentCardsByPet(@Param("petIds") List<Long> petIds, Pageable pageable);
+
 }
