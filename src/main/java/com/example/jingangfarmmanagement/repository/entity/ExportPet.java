2946513302ccba23a9d1c710541f80b53ef0651e
Hoang Duy Dat <hoangduydat10@gmail.com>
2024-07-22 08:18:22 +0700

    update entity ExportPet

--- src/main/java/com/example/jingangfarmmanagement/repository/entity/ExportPet.java
+++ src/main/java/com/example/jingangfarmmanagement/repository/entity/ExportPet.java
@@ -1,8 +1,12 @@
 package com.example.jingangfarmmanagement.repository.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -16,6 +20,8 @@ public class ExportPet extends BaseEntity{
     private Long exportDate;
     private int type;
     private String note;
-
+    @OneToMany(mappedBy = "exportPet", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<ImageExportPet> images;
 
 }
